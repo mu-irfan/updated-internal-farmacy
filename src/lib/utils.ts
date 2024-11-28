38a366cc4267bfc396accc70c6b6b21052b8a300
Muhammad Irfan <irfan.dev34@gmail.com>
2024-11-28 18:49:21 +0500

    fix: updated the base url.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,4 +6,5 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 //base url
-export const baseUrl = "http://192.168.200.46:3036/api";
+// export const baseUrl = "http://192.168.200.46:3036/api";
+export const baseUrl = "https://admin.agronomics.pk/internal/api";
